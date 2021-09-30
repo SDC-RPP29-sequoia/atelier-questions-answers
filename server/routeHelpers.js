d941249362bd23bbdd8a43b75835ede938b09228
Jason <jmolleru@asu.edu>
2021-09-29 20:37:39 -0700

    update models

--- server/routeHelpers.js
+++ server/routeHelpers.js
@@ -1,44 +1,47 @@
 const filterOutReportedQuestions = (doc) => {
   let questions = [];
-  try {
-    doc.questions.forEach((question) => {
-      const answers = filterOutReportedAnswers(question);
-      if (question.reported === 0) {
+  for (let i = 0; i < doc.questions.length; i ++) {
+    let question = doc.questions[i];
+    const answers = filterOutReportedAnswers(question);
+    if (question.reported === 0) {
+      try {
         questions.push({
           'question_id': question.id,
           'question_body': question.body,
-          'question_date': (new Date(question.dateWritten)).toISOString(),
+          'question_date': new Date(question.dateWritten).toISOString(),
           'asker_name': question.askerName,
           'question_helpfulness': question.helpful,
           'reported': false,
           'answers': answers
         });
+      } catch (err) {
+        console.log(`error occurred for question at: ${doc.product_id}`);
       }
-    });
-  } catch (err) {
-    console.log(`error occurred for question at: ${doc.product_id}`);
+    }
   }
   return questions;
 };
 
 const filterOutReportedAnswers = (question) => {
   let answers = {};
-  try {
-    question.answers.forEach((answer) => {
-      if (answer.reported === 0) {
+  for (let i = 0; i < question.answers.length; i++) {
+    let answer = question.answers[i];
+    if (answer.reported === 0) {
+      try {
         answers[answer.id] = {
           'id': answer.id,
           'body': answer.body,
-          'date': (new Date(answer.dateWritten)).toISOString(),
+          'date': new Date(answer.dateWritten).toISOString(),
           'answerer_name': answer.askerName,
           'helpfulness': answer.helpful,
           'photos': answer.photos
         };
+      } catch (err) {
+        console.log(`error occurred for question at ${question.id}`);
       }
-    });
-  } catch (err) {
-    console.log(`error occurred for question at ${question.id}`);
+    }
   }
+
   return answers;
 };
 
